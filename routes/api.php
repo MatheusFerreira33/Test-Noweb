ace2c30e9554a4a2f8008e1bbd53c51ab2711e80
MatheusFerreira33 <matheus.fsilva33@gmail.com>
2023-10-06 18:11:02 -0300

    feat:create route update and delete

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/users', [UserController::class, 'create']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::patch('/users/{id}',  [UserController::class, 'update']);
+Route::delete('/users/{id}',  [UserController::class, 'destroy']);
